a1673dd3f8ceae095858f74d5ffb9d505dabd80a
mikel-js <michaelcastro.js@gmail.com>
2022-07-31 21:44:07 +0300

    feat: add methods

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import QuestionCard from './components/QuestionCard';
 
-function App() {
-  return <div className='App'>Boiler Plate</div>;
-}
+const App = () => {
+  const startQuiz = async () => {};
+
+  const checkAnswer = () => {};
+  // const checkAnswer = (e: MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+  // const nextQuestion = (e: MouseEvent<HTMLButtonElement>) => {};
+  return (
+    <div className='App'>
+      <h1>Quiz Game</h1>
+      <button className='start' onClick={startQuiz}>
+        Start
+      </button>
+      <p className='score'>Score:</p>
+      <p>Loading...</p>
+      <QuestionCard />
+      <button className='next' onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  );
+};
 
 export default App;
