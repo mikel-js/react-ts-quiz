a88dacff3979b4f532398603012a8a9d234c8f7c
mikel-js <michaelcastro.js@gmail.com>
2022-07-31 22:10:54 +0300

    feat: add fetch function

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Difficulty, fetchQuizQuestions } from './API';
 import QuestionCard from './components/QuestionCard';
 
+const TOTAL_QUESTIONS = 10;
+
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswer, setUserAnser] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.MEDIUM));
+
   const startQuiz = async () => {};
 
   const checkAnswer = () => {};
@@ -17,7 +29,14 @@ const App = () => {
       </button>
       <p className='score'>Score:</p>
       <p>Loading...</p>
-      <QuestionCard />
+      {/* <QuestionCard
+        questionNr={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswer ? userAnswer[number] : undefined}
+        callback={checkAnswer}
+      /> */}
       <button className='next' onClick={nextQuestion}>
         Next Question
       </button>
