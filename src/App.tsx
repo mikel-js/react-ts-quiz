e5672403b19684ff81261abedb870f99f55ebad5
mikel-js <michaelcastro.js@gmail.com>
2022-08-02 22:37:32 +0300

    feat: add logic for next button

--- src/App.tsx
+++ src/App.tsx
@@ -60,9 +60,14 @@ const App = () => {
           callback={checkAnswer}
         />
       )}
-      <button className='next' onClick={nextQuestion}>
-        Next Question
-      </button>
+      {!gameOver &&
+      !loading &&
+      userAnswer.length === number + 1 &&
+      number !== TOTAL_QUESTIONS - 1 ? (
+        <button className='next' onClick={nextQuestion}>
+          Next Question
+        </button>
+      ) : null}
     </div>
   );
 };
